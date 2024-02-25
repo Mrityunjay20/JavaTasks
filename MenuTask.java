4ea98b1a0dd4c9fd17ba7a454fa78a6d658d22ca
MJX.SH <70670860+Mrityunjay20@users.noreply.github.com>
2024-02-25 18:21:14 +0530

    Update MenuTask.java

--- MenuTask.java
+++ MenuTask.java
@@ -1,16 +1,35 @@
 package com.edu.users;
-
+/*
+* INPUT:give input as 1,\r\n"
+*                               + "then data shld be inserted\r\n"
+				+ "input is 2\r\n"
+				+ "then data shld be deleted\r\n"
+				+ "input is 3\r\n"
+				+ "then data shld be displayed\r\n"
+				+ "input is 4\r\n"
+				+ "then duplicate values shld be displayed\r\n"
+				+ "input 5 to exit"
+*
+*
+*
+*
+* Author: Mrityunjay Shrivastava 
+* emp id:T00786
+*/
 import java.util.Scanner;
 import java.util.Vector;
 
 public class MenuTask {
 	public static void main(String[] args) {
-		Scanner sca = new Scanner(System.in);
+		Scanner sca = new Scanner(System.in); //INITIATED SCANNER CLASS TO TAKE INPUT
 		
-		Vector objVector = new Vector();
-		
+		Vector objVector = new Vector(); //INITIATED VECTOR OBJECT TO STORE DATA
+
+		// ADD DATA FOR TEST
 		objVector.add("gsdvfgv");
-		System.out.println("give input as 1,\r\n"
+
+		//PRINT MENU OPTIONS
+		System.out.println("give input as 1,\r\n" 
 				+ "then data shld be inserted\r\n"
 				+ "input is 2\r\n"
 				+ "then data shld be deleted\r\n"
@@ -21,13 +40,13 @@ public class MenuTask {
 				+ "input 5 to exit");
 		
 		
-		boolean takeInputs = true;
-		while(takeInputs) {
+		boolean takeInputs = true; //USING A BOOLEAN FLAG TO HELP USER EXIT THE PROGRAM
+		while(takeInputs) {  //USING WHILE LOOP TO ALLOW CONTINOUS INPUTS ONE AFTER OTHER
 			System.out.println("enter input:\n");
 			int input = sca.nextInt();
 			
 		
-			switch (input) {
+			switch (input) { // USE SWITCH CASE TO PROCESS VARIOUS CASES OF THE MENU
 			case 1:
 				System.out.println("enter data:");
 				String dataString = sca.next();
@@ -47,6 +66,7 @@ public class MenuTask {
 				System.out.println("Successfully exited:\n");
 				break;
 			default:
+					// if A WRONG INPUT IS ENTERED USE DEFAULT CASE
 				System.out.println("Wrong input given, give input between 1 to 4\n");
 				break;
 			}
